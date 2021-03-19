bb218d5526a751d3f67e2aeedd0f7e6675091a31
Eugen83 <71218147+Eugen83@users.noreply.github.com>
2021-03-19 23:17:48 +0800

    UserDaoJDBCImpl.java createUsersTable created dropUsersTable created

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,19 +1,51 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private Util util = new Util();
+    private Connection connection = util.creatingConnection();
+    private ResultSet resultSet = null;
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
 
+        try (Statement statement = connection.createStatement()) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            resultSet = metaData.getTables(null, null, "users", null);
+            if (!resultSet.next()) {
+                statement.executeUpdate("CREATE TABLE users(id LONG PRIMARY KEY AUTO INCREMENT, name VARCHAR(45), " +
+                        "last_name VARCHAR(100), age BYTE");
+            } else {
+                System.out.println("The table You're trying to create is already exist!");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+
+        }
+
     }
 
     public void dropUsersTable() {
+        try (Statement statement = connection.createStatement()) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            resultSet = metaData.getTables(null, null, "users", null);
+            if (resultSet.next()) {
+                statement.executeUpdate("DROP TABLE users");
+            } else {
+                System.out.println("there aren't the table You're trying to delete");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+
+        }
 
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,28 +1,35 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?allowPublicKeyRetrieval=true&useSSL=false";
     private static final String PASSWORD = "root";
     private static final String USERNAME = "root";
 
-    private Driver driver;
-    private Connection connection;
+    public Connection creatingConnection() {
 
-    {
-        try {
-            driver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(driver);
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        Driver driver;
+        Connection connection = null;
+     //   Statement statement = null;
+
+
+        {
+            try {
+                driver = new com.mysql.cj.jdbc.Driver();
+                DriverManager.registerDriver(driver);
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+          //      statement = connection.createStatement();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
         }
+        return connection;
     }
-
-
-
 }
+
+
+
+
